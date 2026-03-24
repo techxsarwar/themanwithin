a2d5ea3257889eed90089d4c4a46a48cd49e7fe7
Sarwar Altaf <contact@sarwaraltaf.in>
2026-03-24 19:10:25 +0530

    Update code

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 // Change this to your Render URL after you deploy the backend!
 // Example: const API_BASE_URL = 'https://the-man-within-backend-xyz.onrender.com';
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'https://themanwithin.onrender.com';
